722b7b2ba45d1f6a4c7e380a8da118bc5e49f0a4
Jens Bredigkeit <j.bredigkeit@alexander-buerkle.de>
2020-03-22 13:20:27 +0100

    user scale no

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         {props.headComponents}
       </head>
